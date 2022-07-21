dfcf1b0609509f303b7c04485ce4a1955eb87d9e
Docs builder <info@parity.io>
2022-07-21 22:00:07 +0200

    Deploy updates

---
+++ burnr/src/hooks/api/useLocalStorage.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+export const useLocalStorage = (localStorageKey) => {
+    const [localValue, setLocalValue] = useState(localStorage.getItem(localStorageKey) || "");
+    useEffect(() => {
+        localStorage.setItem(localStorageKey, localValue);
+    }, [localValue, localStorageKey]);
+    return [localValue, setLocalValue];
+};
+//# sourceMappingURL=useLocalStorage.js.map
---
+++ burnr/src/index.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import "regenerator-runtime/runtime";
+import { render } from "react-dom";
+import App from "./App";
+render(_jsx(App, {}), document.getElementById("app"));
+//# sourceMappingURL=index.js.map
