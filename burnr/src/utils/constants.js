45fe6dfcd03561885f6b4e40e39cc26e046ffda3
Docs builder <info@parity.io>
2023-04-20 20:41:49 +0300

    Deploy updates

--- burnr/src/utils/constants.js
+++ burnr/src/utils/constants.js
@@ -3,9 +3,16 @@ export const POLKA_ACCOUNT_ENDPOINTS = {
     polkascan: "polkascan.io",
     polkastats: "polkastats.io",
 };
-export const NETWORK = {
+export const NETWORK = [{
     name: "Westend",
     id: "westend2",
     client: "Light client",
-};
+    lc: true
+}, 
+{
+  name: "Westend",
+  id: "westend2",
+  client: "Light client",
+  lc: true
+}];
 //# sourceMappingURL=constants.js.map
